3ecd6852197b6a26b664410a346ac4a70402c164
suchenkai.kyle@gmail.com <suchenkai.kyle@gmail.com>
2021-10-17 00:17:54 -0400

    Finish all the js lint part

--- submit/insta485/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Insta485 REST API."""
-
-from insta485.api.posts import get_post
-from insta485.api.posts import get_posts
-from insta485.api.posts import get_index
-from insta485.api.posts import create_comment
-from insta485.api.posts import delete_comment
-from insta485.api.posts import create_like
-from insta485.api.posts import delete_like
